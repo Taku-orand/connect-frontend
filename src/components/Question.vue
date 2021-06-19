2c59477a0f1a4376cb17123b83a4b9440f73a502
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-19 16:36:44 +0900

    質問カードデザイン

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,18 +1,30 @@
 <template>
-  <h2>this is question</h2>
+  <h2 class="text-center m-5">質問一覧</h2>
   <!-- <template v-for="question in $store.state.questions"></template> -->
   <div class="card">
     <div class="card-header">
-      質問者<span class="badge badge-secondary p-2">解決済</span>
-      <div></div>
+      <div class="row">
+        <div class="col-6">
+          <div>質問者</div>
+        </div>
+        <div class="col-6 text-right">
+          <span class="badge badge-secondary p-2">解決済</span>
+        </div>
+      </div>
     </div>
     <div class="card-body">
       <h5 class="card-title">タイトル</h5>
       <p class="card-text">質問内容質問内容質問内容質問内容質問内容質問内容質問内容質問内容質問内容</p>
-      <button type="button" class="btn btn-primary">いいね <span class="badge badge-light">5</span></button>
     </div>
     <div class="card-footer text-muted">
-      2021/6/18
+      <div class="row">
+        <div class="col-6">
+          <div class="mt-3">2021/6/18</div>
+        </div>
+        <div class="col-6 text-right">
+          <button type="button" class="btn btn-primary p-2">いいね <span class="badge badge-light">5</span></button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
