6e13af930559d9b52a7444db04b286f46e7217fd
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-23 15:11:58 +0900

    アラート修正

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <h2 class="text-center m-5">マイページ</h2>
     <p>{{ $store.state.user.email }}</p>
+    <QuestionList></QuestionList>
   </div>
 </template>
 
@@ -11,9 +12,13 @@ import { reactive, onMounted } from "vue";
 // import { useRoute } from "vue-router";
 // import { useStore } from "vuex";
 
+import QuestionList from "./QuestionList.vue";
+
 export default {
   name: "MyPage",
-  components: {},
+  components: {
+    QuestionList,
+  },
   props: {},
   setup() {
     // const router = useRouter();
