d7ca40f0f4936c4a5c9e0915961292373d1494df
a-kit-2 <akito.nakamura.c@gmail.com>
2021-06-30 16:28:10 +0900

    taglist修正

--- src/components/QuestionList.vue
+++ src/components/QuestionList.vue
@@ -1,41 +1,34 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-3">
-        <TagList></TagList>
-      </div>
-      <div class="col-9">
-        <h2 class="text-center m-4">質問一覧</h2>
-        <template v-for="(question, key) in $store.state.questions" :key="key">
-          <div @click="showDetail(question)" class="card mb-4">
-            <div class="card-header">
-              <div class="row">
-                <div class="col-6">
-                  <div>質問者</div>
-                </div>
-                <div class="col-6 text-right">
-                  <span v-if="question.solved" class="badge badge-secondary p-2">解決済</span>
-                </div>
-              </div>
+    <h2 class="text-center m-4">質問一覧</h2>
+    <template v-for="(question, key) in $store.state.questions" :key="key">
+      <div @click="showDetail(question)" class="card mb-4">
+        <div class="card-header">
+          <div class="row">
+            <div class="col-6">
+              <div>質問者</div>
             </div>
-            <div class="card-body">
-              <h5 class="card-title">{{ question.title }}</h5>
-              <p class="card-text">{{ question.content }}</p>
-            </div>
-            <div class="card-footer text-muted">
-              <div class="row">
-                <div class="col-6">
-                  <div class="mt-2">{{ question.updated_at }}</div>
-                </div>
-                <div class="col-6 text-right">
-                  <LikeButton :question="question" :is-my-page="false"></LikeButton>
-                </div>
-              </div>
+            <div class="col-6 text-right">
+              <span v-if="question.solved" class="badge badge-secondary p-2">解決済</span>
             </div>
           </div>
-        </template>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{{ question.title }}</h5>
+          <p class="card-text">{{ question.content }}</p>
+        </div>
+        <div class="card-footer text-muted">
+          <div class="row">
+            <div class="col-6">
+              <div class="mt-2">{{ question.updated_at }}</div>
+            </div>
+            <div class="col-6 text-right">
+              <LikeButton :question="question" :is-my-page="false"></LikeButton>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -45,14 +38,12 @@ import { reactive, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
 
-import TagList from "./TagList.vue";
 import LikeButton from "./LikeButton.vue";
 
 export default {
   name: "QuestionList",
 
   components: {
-    TagList,
     LikeButton,
   },
   props: {
