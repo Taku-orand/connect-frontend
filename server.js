f1d4702b137aaefb5dfd0034b9041b63c278f342
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-17 14:56:23 +0900

    他のURL対応

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+var express = require("express");
+var path = require("path");
+var serveStatic = require("serve-static");
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+console.log("server started " + port);
