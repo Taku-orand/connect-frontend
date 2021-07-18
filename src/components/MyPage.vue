785f753ac348e1b317d22392be676af64f3343b8
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-18 15:09:36 +0900

    mypage修正

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -5,7 +5,7 @@
       <div class="card-body pb-0">
         <p class="mb-3 text-center"><i class="fas fa-user mr-2"></i>ユーザー名: {{ $store.state.user.name }}</p>
         <p class="mb-3 text-center"><i class="fas fa-envelope mr-2"></i>メールアドレス: {{ $store.state.user.email }}</p>
-        <p class="mb-3 text-center"><i class="fas fa-thumbs-up mr-2"></i>合計いいね: {{ $store.getters.getLikeSum }}</p>
+        <p class="mb-3 text-center"><i class="fas fa-thumbs-up mr-2"></i>合計いいね: {{ data.likeSum }}</p>
       </div>
     </div>
     <QuestionList :is-my-page="true"></QuestionList>
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import { reactive, onMounted } from "vue";
+import { reactive, onMounted, onUpdated } from "vue";
 // import axios from "axios";
 // import { useRoute } from "vue-router";
 import { useStore } from "vuex";
@@ -35,9 +35,13 @@ export default {
       likeSum: 0,
     });
 
-    onMounted(() => {
-      store.dispatch("checkSignedIn");
+    onMounted(async () => {
+      await store.dispatch("checkSignedIn");
+      await store.dispatch("getMyQuestion");
+      data.likeSum = store.getters.getLikeSum;
     });
+
+    onUpdated(() => {});
 
     return {
       data,
