d75662801a4933ce7379e96f3f5e0f445f579dee
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-19 18:13:53 +0900

    google 修正

--- src/main.js
+++ src/main.js
@@ -2,13 +2,10 @@ import { createApp } from "vue";
 import { router } from "./router.js";
 import { store } from "./store.js";
 import App from "./App.vue";
-import VueAnalytics from "vue-analytics";
+import VueGtag from "vue-gtag-next";
 
 var app = createApp(App);
 app.use(router);
 app.use(store);
-app.use(VueAnalytics, {
-  id: "G-L0EN9QCWZ3",
-  router,
-});
+app.use(VueGtag, { property: { id: "G-L0EN9QCWZ3", router } });
 app.mount("#app");
