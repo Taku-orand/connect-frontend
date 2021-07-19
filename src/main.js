3e75851a5b519ef451dad42e07325672f9f96a78
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-19 18:02:37 +0900

    google アナリティクス導入

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import { createApp } from "vue";
 import { router } from "./router.js";
 import { store } from "./store.js";
 import App from "./App.vue";
+import VueAnalytics from "vue-analytics";
 
 var app = createApp(App);
 app.use(router);
 app.use(store);
+app.use(VueAnalytics, {
+  id: "G-L0EN9QCWZ3",
+  router,
+});
 app.mount("#app");
